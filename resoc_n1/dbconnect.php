a28c92b579e72e8feedd1830a84cdd1b89b1aa7a
Julie <jem.baret@gmail.com>
2022-10-04 17:28:39 +0200

    add: info.text // In news.php --> add: inject PHP into HTML to display 5 last posts / comment: SQL request and PHP functions

--- resoc_n1/dbconnect.php
+++ resoc_n1/dbconnect.php
@@ -13,7 +13,7 @@ if ($mysqli->connect_errno)
 {
     echo "<article>";
     echo("Échec de la connexion : " . $mysqli->connect_error);
-    echo("<p>Indice: Vérifiez les parametres de <code>new mysqli(...</code></p>");
+    // echo("<p>Indice: Vérifiez les parametres de <code>new mysqli(...</code></p>");
     echo "</article>";
     exit();
 
