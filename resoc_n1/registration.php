f4251b1b73902dfa1284c96bc78f38f56536078d
Sandrine Le Diraison <didine.perla@gmail.com>
2022-10-13 11:54:28 +0200

    in progress: do not register if fields are empty

--- resoc_n1/registration.php
+++ resoc_n1/registration.php
@@ -33,37 +33,40 @@
                         $new_email = $_POST['email'];
                         $new_alias = $_POST['pseudo'];
                         $new_passwd = $_POST['motpasse'];
-
-
-                        //Etape 3 : Ouvrir une connexion avec la base de donnée.
-                        include_once('dbconnect.php');
-                        //Etape 4 : Petite sécurité
-                        // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
-                        $new_email = $mysqli->real_escape_string($new_email);
-                        $new_alias = $mysqli->real_escape_string($new_alias);
-                        $new_passwd = $mysqli->real_escape_string($new_passwd);
-                        // on crypte le mot de passe pour éviter d'exposer notre utilisatrice en cas d'intrusion dans nos systèmes
-                        $new_passwd = md5($new_passwd);
-                        // NB: md5 est pédagogique mais n'est pas recommandée pour une vraies sécurité
-                        //Etape 5 : construction de la requete
-                        $lInstructionSql = "INSERT INTO users (id, email, password, alias) "
-                                . "VALUES (NULL, "
-                                . "'" . $new_email . "', "
-                                . "'" . $new_passwd . "', "
-                                . "'" . $new_alias . "'"
-                                . ");";
-                        // Etape 6: exécution de la requete
-                        $ok = $mysqli->query($lInstructionSql);
-                        if ( ! $ok)
-                        {
-                            echo "L'inscription a échouée : " . $mysqli->error;
-                        } else
-                        {
-                            echo "Votre inscription est un succès : " . $new_alias;
-                            echo " <a href='login.php'>Connectez-vous.</a>";
+                        if($new_email=="" OR $new_alias=="" OR $new_passwd==""){
+                            echo "<h4> Vous devez remplir tous les champs. </h4>";
+                        } else {
+                            //Etape 3 : Ouvrir une connexion avec la base de donnée.
+                            include_once('dbconnect.php');
+                            //Etape 4 : Petite sécurité
+                            // pour éviter les injection sql : https://www.w3schools.com/sql/sql_injection.asp
+                            $new_email = $mysqli->real_escape_string($new_email);
+                            $new_alias = $mysqli->real_escape_string($new_alias);
+                            $new_passwd = $mysqli->real_escape_string($new_passwd);
+                            // on crypte le mot de passe pour éviter d'exposer notre utilisatrice en cas d'intrusion dans nos systèmes
+                            $new_passwd = md5($new_passwd);
+                            // NB: md5 est pédagogique mais n'est pas recommandée pour une vraies sécurité
+                            //Etape 5 : construction de la requete
+                            $lInstructionSql = "INSERT INTO users (id, email, password, alias) "
+                                    . "VALUES (NULL, "
+                                    . "'" . $new_email . "', "
+                                    . "'" . $new_passwd . "', "
+                                    . "'" . $new_alias . "'"
+                                    . ");";
+                            // Etape 6: exécution de la requete
+                            $ok = $mysqli->query($lInstructionSql);
+                            if ( ! $ok)
+                            {
+                                echo "L'inscription a échouée : " . $mysqli->error;
+                            } else
+                            {
+                                echo "Votre inscription est un succès : " . $new_alias;
+                                echo " <a href='login.php'>Connectez-vous.</a>";
+                            }
                         }
                     }
                     ?>                     
+
                     <form action="registration.php" method="post">
                         <dl>
                             <dt><label for='pseudo'>Pseudo</label></dt>
